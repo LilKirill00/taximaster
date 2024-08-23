63a0b7db26fc664e17c9eded933b4ad935b47f54
LilKirill00 <pc.browsers.1@gmail.com>
2024-08-23 13:20:04 +0500

    refactor: актуализация "Создание экипажа"

--- common_api/create_crew.go
+++ common_api/create_crew.go
@@ -13,8 +13,6 @@ type (
 
 		// Позывной экипажа
 		Code string `json:"code,omitempty" validate:"omitempty"`
-		// Используется списания за смену
-		UseShifts bool `json:"use_shifts,omitempty" validate:"omitempty"`
 		// Сумма, списываемая за смену
 		WorkShiftSum float64 `json:"work_shift_sum,omitempty" validate:"omitempty"`
 		// Минимальный баланс, при котором можно выйти на смену
@@ -34,17 +32,16 @@ type (
 	}
 
 	CreateCrewResponse struct {
+		// ИД созданного экипажа
 		CrewID int `json:"crew_id"`
 	}
 )
 
 // Создание экипажа
-func (cl *Client) CreateCrew(req CreateCrewRequest) (CreateCrewResponse, error) {
-	var response = CreateCrewResponse{}
-
-	err := validator.Validate(req)
+func (cl *Client) CreateCrew(req CreateCrewRequest) (response CreateCrewResponse, err error) {
+	err = validator.Validate(req)
 	if err != nil {
-		return response, err
+		return
 	}
 
 	/*
@@ -66,5 +63,5 @@ func (cl *Client) CreateCrew(req CreateCrewRequest) (CreateCrewResponse, error) 
 
 	err = cl.PostJson("create_crew", e, req, &response)
 
-	return response, err
+	return
 }
