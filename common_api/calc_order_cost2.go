7218c6d644066af746d453eb934bb125a88ff653
LilKirill00 <pc.browsers.1@gmail.com>
2024-08-23 12:24:32 +0500

    refactor: актуализация "Расчет суммы заказа 2"

--- common_api/calc_order_cost2.go
+++ common_api/calc_order_cost2.go
@@ -4,6 +4,8 @@ import "github.com/ros-tel/taximaster/validator"
 
 type (
 	CalcOrderCost2Request struct {
+		// ИД заказа
+		OrderID *int `json:"order_id,omitempty" validate:"omitempty"`
 		// ИД тарифа
 		TariffID *int `json:"tariff_id,omitempty" validate:"omitempty"`
 		// Время подачи
@@ -27,9 +29,9 @@ type (
 		// ИД района назначения
 		DestZoneID *int `json:"dest_zone_id" validate:"omitempty"`
 		// Долгота адреса назначения
-		DestinationLon *float64 `json:"dest_lon,omitempty" validate:"omitempty"`
+		DestLon *float64 `json:"dest_lon,omitempty" validate:"omitempty"`
 		// Широта адреса назначения
-		DestinationLat *float64 `json:"dest_lat,omitempty" validate:"omitempty"`
+		DestLat *float64 `json:"dest_lat,omitempty" validate:"omitempty"`
 		// Километраж по городу
 		DistanceCity *float64 `json:"distance_city,omitempty" validate:"omitempty"`
 		// Километраж за городом
@@ -48,42 +50,51 @@ type (
 		IsPrize bool `json:"is_prize,omitempty" validate:"omitempty"`
 		// Обратный путь за городом
 		BackWay bool `json:"back_way,omitempty" validate:"omitempty"`
+		// Массив параметров заказа. Устарело. Рекомендуется использовать параметр attribute_values
+		OrderParams *[]int `json:"order_params,omitempty" validate:"omitempty"`
 		// Признак безналичного заказа
 		Cashless bool `json:"cashless,omitempty" validate:"omitempty"`
+		// Список остановок
+		Stops *[]struct {
+			// ИД района остановки
+			ZoneID int `json:"zone_id"`
+			// Широта адреса остановки
+			Lat float64 `json:"lat"`
+			// Долгота адреса остановки
+			Lon float64 `json:"lon"`
+		} `json:"stops,omitempty" validate:"omitempty"`
 		// ИД группы экипажа
 		CrewGroupID int `json:"crew_group_id,omitempty" validate:"omitempty"`
+		// ИД службы ЕДС
+		UdsID *int `json:"uds_id,omitempty" validate:"omitempty"`
 		// Нужно ли выполнять анализ адресов и маршрута.
 		// Если данный флаг установлен (analyze_route=true), то значения параметров: distance_city, distance_country, source_distance_country, переданные в данном запросе будут игнорироваться.
 		// Они автоматически будут рассчитаны в ходе выполнения запроса в результате анализа адресов и маршрута.
 		// Также перед анализом адресов будут автоматически найдены районы (по справочнику "Районы") для тех адресов, у которых район не указан явно (zone_id=0).
 		// Также по результатам анализа адресов автоматически будут определены флаги "Загородный заказ" (is_country) и "Межгород".
 		AnalyzeRoute bool `json:"analyze_route,omitempty" validate:"omitempty"`
-		// Массив параметров заказа. Устарело. Рекомендуется использовать параметр attribute_values
-		OrderParams *[]int `json:"order_params,omitempty" validate:"omitempty"`
-		// ИД службы ЕДС
-		UdsID *int `json:"uds_id,omitempty" validate:"omitempty"`
-		// Список остановок
-		Stops *[]Stop `json:"stops,omitempty" validate:"omitempty"`
 		// Массив значений атрибутов
 		AttributeValues *[]AttributeValue `json:"attribute_values,omitempty" validate:"omitempty"`
 	}
 
 	CalcOrderCost2Response struct {
-		Sum  float64 `json:"sum"`
+		// Рассчитанная общая сумма заказа
+		Sum float64 `json:"sum"`
+		// Дополнительная информация по расчету суммы заказа
 		Info []struct {
+			// Описание позиции дополнительной информации по расчету суммы заказа
 			Comment string `json:"comment"`
-			Sum     string `json:"sum"`
+			// Сумма позиции дополнительной информации по расчету суммы заказа
+			Sum string `json:"sum"`
 		} `json:"info"`
 	}
 )
 
 // Расчет суммы заказа 2
-func (cl *Client) CalcOrderCost2(req CalcOrderCost2Request) (CalcOrderCost2Response, error) {
-	var response = CalcOrderCost2Response{}
-
-	err := validator.Validate(req)
+func (cl *Client) CalcOrderCost2(req CalcOrderCost2Request) (response CalcOrderCost2Response, err error) {
+	err = validator.Validate(req)
 	if err != nil {
-		return response, err
+		return
 	}
 
 	/*
@@ -101,6 +112,7 @@ func (cl *Client) CalcOrderCost2(req CalcOrderCost2Request) (CalcOrderCost2Respo
 		111	Сотрудник клиента не найден
 		112	Атрибут не найден
 		113	Атрибут не может быть привязан к заказу
+		114	Заказ не найден
 	*/
 	e := errorMap{
 		100: ErrTariffNotFound,
@@ -117,9 +129,10 @@ func (cl *Client) CalcOrderCost2(req CalcOrderCost2Request) (CalcOrderCost2Respo
 		111: ErrCustomerClientNotFound,
 		112: ErrAttributeNotFound,
 		113: ErrAttributeCannotBeBoundOrder,
+		114: ErrOrderNotFound,
 	}
 
 	err = cl.PostJson("calc_order_cost2", e, req, &response)
 
-	return response, err
+	return
 }
