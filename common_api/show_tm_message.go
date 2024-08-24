80e9985e83e0977b67549cceef440ee7d43c9e38
LilKirill00 <pc.browsers.1@gmail.com>
2024-08-24 09:54:43 +0500

    refactor: актулизация "Показать сообщение в ТМ"

--- common_api/show_tm_message.go
+++ common_api/show_tm_message.go
@@ -7,22 +7,34 @@ type (
 		// Текст сообщения
 		Text string `json:"text" validate:"required"`
 
+		// Тип сообщения ("warning", "error", "information", "confirmation"), по умолчанию "information"
+		Type string `json:"type,omitempty" validate:"omitempty"`
 		// Заголовок сообщения
 		Header string `json:"header,omitempty" validate:"omitempty"`
 		// Скрывать сообщение через, сек. (0 — не скрывать)
 		Timeout int `json:"timeout" validate:"omitempty"`
 		// Массив пользователей (если не указаны — отправлять всем)
 		Users *[]int `json:"users,omitempty" validate:"omitempty"`
+		// Цвет уведомления в формате RGB: #FFFFFF
+		Color string `json:"color,omitempty" validate:"omitempty,iscolor=hexcolor"`
+		// ИД заказа для кнопки открытия карточки в уведомлении
+		OrderID int `json:"order_id,omitempty" validate:"omitempty"`
+		// ИД автомобиля для кнопки открытия карточки в уведомлении
+		CarID int `json:"car_id,omitempty" validate:"omitempty"`
+		// ИД водителя для кнопки открытия карточки в уведомлении
+		DriverID int `json:"driver_id,omitempty" validate:"omitempty"`
+		// ИД экипажа для кнопки открытия карточки в уведомлении
+		CrewID int `json:"crew_id,omitempty" validate:"omitempty"`
+		// ИД клиента для кнопки открытия карточки в уведомлении
+		ClientID int `json:"client_id,omitempty" validate:"omitempty"`
 	}
 )
 
 // Показать сообщение в ТМ
-func (cl *Client) ShowTmMessage(req ShowTmMessageRequest) (EmptyResponse, error) {
-	var response = EmptyResponse{}
-
-	err := validator.Validate(req)
+func (cl *Client) ShowTmMessage(req ShowTmMessageRequest) (response EmptyResponse, err error) {
+	err = validator.Validate(req)
 	if err != nil {
-		return response, err
+		return
 	}
 
 	/*
@@ -34,5 +46,5 @@ func (cl *Client) ShowTmMessage(req ShowTmMessageRequest) (EmptyResponse, error)
 
 	err = cl.PostJson("show_tm_message", e, req, &response)
 
-	return response, err
+	return
 }
